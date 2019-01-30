1790e2dd127d3e29584d72227ec772c8067d1ce5
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-01-29 17:36:54 -0800

    Initial implementation of k8s-tiller

---
+++ modules/k8s-tiller/main.tf
@@ -0,0 +1,58 @@
+// TODO: When we have package-terraform-utilities available
+// - Implement multiline shell commands by selecting the escape character based on platform (\ for unix, ^ for windows)
+// - Implement command required
+
+locals {
+  helm_home_with_default = "${var.helm_home == "" ? pathexpand("~/.helm") : var.helm_home}"
+  kubectl_config_options = "${var.kubectl_config_context_name != "" ? "--kubectl-context-name ${var.kubectl_config_context_name}" : ""} ${var.kubectl_config_path != "" ? "--kubeconfig ${var.kubectl_config_path}" : ""}"
+
+  tls_config           = "--tls-private-key-algorithm ${var.private_key_algorithm} ${local.tls_algorithm_config} --tls-common-name ${lookup(var.tls_subject, "common_name")} --tls-org ${lookup(var.tls_subject, "org")} ${local.tls_org_unit} ${local.tls_city} ${local.tls_state} ${local.tls_country}"
+  tls_algorithm_config = "${var.private_key_algorithm == "ECDSA" ? "--tls-private-key-ecdsa-curve ${var.private_key_ecdsa_curve}" : "--tls-private-key-rsa-bits ${var.private_key_rsa_bits}"}"
+  tls_org_unit         = "${lookup(var.tls_subject, "org_unit", "") != "" ? "--tls-org-unit ${lookup(var.tls_subject, "org_unit", "")}" : ""}"
+  tls_city             = "${lookup(var.tls_subject, "city", "")     != "" ? "--tls-city ${lookup(var.tls_subject, "city", "")}"         : ""}"
+  tls_state            = "${lookup(var.tls_subject, "state", "")    != "" ? "--tls-state ${lookup(var.tls_subject, "state", "")}"       : ""}"
+  tls_country          = "${lookup(var.tls_subject, "country", "")  != "" ? "--tls-country ${lookup(var.tls_subject, "country", "")}"   : ""}"
+
+  undeploy_args = "${var.force_undeploy ? "--force" : ""} ${var.undeploy_releases ? "--undeploy-releases" : ""}"
+
+  grant_args                 = "${local.rbac_users_args} ${local.rbac_groups_args} ${local.rbac_service_accounts_args}"
+  rbac_users_args            = "${join(" ", formatlist("--rbac-user %s", var.grant_access_to_rbac_users))}"
+  rbac_groups_args           = "${join(" ", formatlist("--rbac-group %s", var.grant_access_to_rbac_groups))}"
+  rbac_service_accounts_args = "${join(" ", formatlist("--rbac-service-account %s", var.grant_access_to_rbac_service_accounts))}"
+
+  configure_args = "${
+    var.helm_client_rbac_user != "" ? "--rbac-user ${var.helm_client_rbac_user}" 
+      : var.helm_client_rbac_group != "" ? "--rbac-group ${var.helm_client_rbac_group}"
+        : var.helm_client_rbac_service_account != "" ? "--rbac-service-account ${var.helm_client_rbac_service_account}"
+          : ""
+  }"
+}
+
+resource "null_resource" "tiller" {
+  provisioner "local-exec" {
+    command = "kubergrunt helm deploy ${local.kubectl_config_options} --service-account ${var.service_account} --tiller-namespace ${var.tiller_namespace} ${local.tls_config}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubergrunt helm undeploy ${local.kubectl_config_options} --home ${local.helm_home_with_default} --tiller-namespace ${var.tiller_namespace} ${local.undeploy_args}"
+    when    = "destroy"
+  }
+}
+
+resource "null_resource" "grant_access_to_tiller" {
+  count = "${trimspace(local.grant_args) == "" ? 0 : 1}"
+
+  provisioner "local-exec" {
+    command = "kubergrunt helm grant ${local.kubectl_config_options} --tiller-namespace ${var.tiller_namespace} ${local.tls_config} ${local.grant_args}"
+  }
+
+  depends_on = ["null_resource.tiller"]
+}
+
+resource "null_resource" "configure_local_helm_client" {
+  count = "${local.configure_args == "" ? 0 : 1}"
+
+  provisioner "local-exec" {
+    command = "kubergrunt helm configure ${local.kubectl_config_options} --home ${local.helm_home_with_default} --tiller-namespace ${var.tiller_namespace} --resource-namespace ${var.resource_namespace} --set-kubectl-namespace ${local.configure_args}"
+  }
+}
--- modules/k8s-tiller/variables.tf
+++ modules/k8s-tiller/variables.tf
@@ -16,7 +16,7 @@ variable "service_account" {
 }
 
 variable "tls_subject" {
-  description = "The issuer information that contains the identifying information for the Tiller server. Used to generate the TLS certificate keypairs."
+  description = "The issuer information that contains the identifying information for the Tiller server. Used to generate the TLS certificate keypairs. common_name and org are required."
   type        = "map"
 
   # Expects the following keys
@@ -51,12 +51,65 @@ variable "grant_access_to_rbac_service_accounts" {
   default     = []
 }
 
+# TLS configuration
+
+variable "private_key_algorithm" {
+  description = "The name of the algorithm to use for private keys. Must be one of: RSA or ECDSA."
+  default     = "ECDSA"
+}
+
+variable "private_key_ecdsa_curve" {
+  description = "The name of the elliptic curve to use. Should only be used if var.private_key_algorithm is ECDSA. Must be one of P224, P256, P384 or P521."
+  default     = "P256"
+}
+
+variable "private_key_rsa_bits" {
+  description = "The size of the generated RSA key in bits. Should only be used if var.private_key_algorithm is RSA."
+  default     = "2048"
+}
+
+# Kubernetes auth configuration
+
 variable "kubectl_config_context_name" {
   description = "The config context to use when authenticating to the Kubernetes cluster. If empty, defaults to the current context specified in the kubeconfig file."
-  value       = ""
+  default     = ""
 }
 
 variable "kubectl_config_path" {
   description = "The path to the config file to use for kubectl. If empty, defaults to $HOME/.kube/config"
-  value       = ""
+  default     = ""
+}
+
+variable "helm_home" {
+  description = "The path to the home directory for helm that you wish to use for this deployment."
+  default     = ""
+}
+
+# Setup helm client. If any of the following is set, will setup the helm client with that entity.
+
+variable "helm_client_rbac_user" {
+  description = "If set, will setup the local helm client to authenticate using this RBAC user. The RBAC user must be in the grant_access_to_rbac_users list."
+  default     = ""
+}
+
+variable "helm_client_rbac_group" {
+  description = "If set, will setup the local helm client to authenticate using this RBAC group. The RBAC group must be in the grant_access_to_rbac_groups list."
+  default     = ""
+}
+
+variable "helm_client_rbac_service_account" {
+  description = "If set, will setup the local helm client to authenticate using this ServiceAccount. The ServiceAccount must be in the grant_access_to_rbac_service_accounts list."
+  default     = ""
+}
+
+# Undeploy options
+
+variable "force_undeploy" {
+  description = "If true, will remove the Tiller server resources even if there are releases deployed."
+  default     = false
+}
+
+variable "undeploy_releases" {
+  description = "If true, will delete deployed releases from the Tiller instance before undeploying Tiller."
+  default     = false
 }
