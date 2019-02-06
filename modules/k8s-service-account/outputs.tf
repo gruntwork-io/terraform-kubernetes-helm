dcece851d2c6a18c5ac8a6cec924feb16e9055fd
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-02-06 12:03:28 -0800

    Bugfix and tweaks

--- modules/k8s-service-account/outputs.tf
+++ modules/k8s-service-account/outputs.tf
@@ -1,4 +1,6 @@
 output "name" {
   description = "The name of the created service account"
   value       = "${kubernetes_service_account.service_account.metadata.0.name}"
+
+  depends_on = ["kubernetes_role_binding.service_account_role_binding"]
 }
