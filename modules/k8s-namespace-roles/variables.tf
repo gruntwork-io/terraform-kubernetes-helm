c478a7b2bab903452a0e7ccd829ffbd303f99e6e
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-10-21 10:42:26 -0700

    Update documentation for create_resources

--- modules/k8s-namespace-roles/variables.tf
+++ modules/k8s-namespace-roles/variables.tf
@@ -26,7 +26,7 @@ variable "annotations" {
 }
 
 variable "create_resources" {
-  description = "Set to false to have this module create no resources. This weird parameter exists solely because Terraform does not support conditional modules. Therefore, this is a hack to allow you to conditionally decide if the Namespace roles should be created or not."
+  description = "Set to false to have this module skip creating resources. This weird parameter exists solely because Terraform does not support conditional modules. Therefore, this is a hack to allow you to conditionally decide if the Namespace roles should be created or not."
   type        = bool
   default     = true
 }
--- modules/k8s-service-account/variables.tf
+++ modules/k8s-service-account/variables.tf
@@ -69,7 +69,7 @@ variable "secrets_for_pods" {
 }
 
 variable "create_resources" {
-  description = "Set to false to have this module create no resources. This weird parameter exists solely because Terraform does not support conditional modules. Therefore, this is a hack to allow you to conditionally decide if the Namespace should be created or not."
+  description = "Set to false to have this module skip creating resources. This weird parameter exists solely because Terraform does not support conditional modules. Therefore, this is a hack to allow you to conditionally decide if the Namespace should be created or not."
   type        = bool
   default     = true
 }
